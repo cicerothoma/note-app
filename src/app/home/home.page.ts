75f58aa145918e72e0648f0f32ad2e10934db779
swali <thomascollins582@gmail.com>
2020-01-31 09:21:53 +0100

    App Done

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+import { NotesService } from '../services/notes.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private notesService: NotesService, private alertCtrl: AlertController, private navCtrl: NavController) { }
+
+  ngOnInit() {
+    this.notesService.load();
+  }
+
+  addNote(){
+    this.alertCtrl.create({
+      header: 'New Note',
+      message : 'Input Note Title',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.notesService.createNote(data.title);
+          }
+        }
+      ]
+    }).then((alert) => {
+      alert.present();
+    });
+  }
+
+}
